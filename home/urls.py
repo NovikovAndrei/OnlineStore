6fa4b814892c34c11259859f6d2c1af937155d2d
Andrei_Novikov <novikovas1@mail.ru>
2023-06-06 09:50:40 +0300

    реализован переход на стартовую страницу при нажатии на "название магазина"

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import IndexShopView
 
+app_name = "home"
+
 urlpatterns = [
-    path('', IndexShopView.as_view()),
+    path('', IndexShopView.as_view(), name='index',)
 ]
