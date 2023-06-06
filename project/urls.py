9cb83638b0ebf56fa90fdf6c6fdc1786a3515945
Andrei_Novikov <novikovas1@mail.ru>
2023-06-07 00:15:10 +0300

    Добавлено приложение blog

--- project/urls.py
+++ project/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('cart/', include('cart.urls')),
     path('shop/', include('shop.urls')),
-    path('checkout/', include('checkout.urls'))
+    path('checkout/', include('checkout.urls')),
+    path('blog/', include('blog.urls')),
 ]
