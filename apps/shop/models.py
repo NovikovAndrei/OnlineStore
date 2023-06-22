001a3bf4761ffd3683cfb236adc70a1ccee5d647
Andrei_Novikov <novikovas1@mail.ru>
2023-06-22 09:00:42 +0300

    + content

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='static/category', null=True, blank=True)
 
     def __str__(self):
         return self.name
