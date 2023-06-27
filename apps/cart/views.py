466dae4a4b5ac3a605d8f0063636098bedd21b7f
Andrei_Novikov <novikovas1@mail.ru>
2023-06-28 00:31:03 +0300

    + функционал добавления в корзину

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,10 +1,48 @@
 from django.shortcuts import render
 from django.views import View
-
+from .models import Cart, Product
+from django.db.models import F
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
 
 class CartView(View):
+    # def get(self, request):
+    #     return render(request, 'cart/cart.html')
+
     def get(self, request):
-        return render(request, 'cart/cart.html')
+        cart = Cart.objects.filter(user=request.user).annotate(
+            total_price=F('product__price') * F('quantity')
+        )
+
+        return render(request, 'cart/cart.html', {"data": cart})
+
+class CartViewBuy(View):
+        def get(self, request, product_id):
+            product = get_object_or_404(Product, id=product_id)
+            user = get_object_or_404(User, id=request.user.id)
+            cart_items = Cart.objects.filter(user=user, product=product)
+            if cart_items:
+                cart_item = cart_items[0]
+                cart_item.quantity += 1
+            else:
+                cart_item = Cart(user=user, product=product)
+            cart_item.save()
+            return redirect('cart:cart')
+
+
+class CartViewAdd(View):
+    def get(self, request, product_id):
+        product = get_object_or_404(Product, id=product_id)
+        user = get_object_or_404(User, id=request.user.id)
+        cart_items = Cart.objects.filter(user=user, product=product)
+        if cart_items:
+            cart_item = cart_items[0]
+            cart_item.quantity += 1
+        else:
+            cart_item = Cart(user=user, product=product)
+        cart_item.save()
+        return redirect('shop:shop')
 
 
 class WishlistView(View):
--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import ShopView, ProductSingleView
+from apps.cart.views import CartViewBuy, CartViewAdd
 
 app_name = "shop"
 
 urlpatterns = [
     path('', ShopView.as_view(), name='shop'),
     path('product/<int:product_id>', ProductSingleView.as_view(), name='productsingle'),
-    path('product/', ProductSingleView.as_view(), name='productsingle')
+    path('product/', ProductSingleView.as_view(), name='productsingle'),
+    path('buy/<int:product_id>', CartViewBuy.as_view(), name='buy'),
+    path('add/<int:product_id>', CartViewAdd.as_view(), name='add'),
+
 ]
