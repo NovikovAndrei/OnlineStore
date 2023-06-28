b7765b73165a7f69c0840c22c0f273b6cf076ec6
Andrei_Novikov <novikovas1@mail.ru>
2023-06-28 16:47:52 +0300

    + функционал удаления из корзины и корректный подсчет общей стоимости в корзине

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CartView, WishlistView
+from .views import CartView, WishlistView, CartViewDel
 
 app_name = "cart"
 
 urlpatterns = [
     path('', CartView.as_view(), name='cart'),
-    path('wishlist', WishlistView.as_view(), name='wishlist')
+    path('wishlist', WishlistView.as_view(), name='wishlist'),
+    path('del/<int:product_id>', CartViewDel.as_view(), name='del'),
 
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,21 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .models import Cart, Product
-from django.db.models import F
+from django.db.models import F, OuterRef, Subquery, DecimalField, ExpressionWrapper, Sum, Case, When
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse
+from django.utils import timezone
+
 
 class CartView(View):
-    # def get(self, request):
-    #     return render(request, 'cart/cart.html')
-
     def get(self, request):
-        cart = Cart.objects.filter(user=request.user).annotate(
-            total_price=F('product__price') * F('quantity')
+        user_cart = Cart.objects.filter(user=request.user). \
+            select_related('product')
+
+        total_discount = Case(When(product__discount__value__gte=0,
+                                   product__discount__date_begin__lte=timezone.now(),
+                                   product__discount__date_end__gte=timezone.now(),
+                                   then=F('total_price') * F('product__discount__value') / 100),
+                              default=0,
+                              output_field=DecimalField(max_digits=10, decimal_places=2)
+        )
+        cart = user_cart.annotate(
+            total_price=F('product__price') * F('quantity'),
+            total_discount=total_discount,
+            total_price_with_discount=F('total_price') - F('total_discount'),
         )
 
-        return render(request, 'cart/cart.html', {"data": cart})
+        sum_data = cart.aggregate(sum_price=Sum('total_price'),
+                                  sum_discount=Sum('total_discount'),
+                                  sum_price_with_discount=Sum('total_price_with_discount'))
+
+        context = {"data": cart}
+        context.update(sum_data)
+
+        return render(request, 'cart/cart.html', context)
 
 class CartViewBuy(View):
         def get(self, request, product_id):
@@ -43,6 +59,13 @@ class CartViewAdd(View):
             cart_item = Cart(user=user, product=product)
         cart_item.save()
         return redirect('shop:shop')
+
+class CartViewDel(View):
+  def get(self, request, product_id):
+
+      cart_item = Cart.objects.get(user=request.user, product__id=product_id)
+      cart_item.delete()
+      return redirect('cart:cart')
 
 
 class WishlistView(View):
