86ac8267ad7f40eac124f0f2e173f62ea300d425
Andrei_Novikov <novikovas1@mail.ru>
2023-07-09 13:51:07 +0300

    + в комментариях добавлена возможность удаления (удалять можно только свои комментарии)

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import BlogView, BlogSingleView
+from .views import BlogView, BlogSingleView, DeleteCommentView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', BlogView.as_view(), name='blog'),
     path('blogsingle/<int:article_id>', BlogSingleView.as_view(), name='blogsingle'),
-    path('blogsingle', BlogSingleView.as_view(), name='blogSingle')
+    path('blogsingle', BlogSingleView.as_view(), name='blogSingle'),
+    path('delete_comment/<int:comment_id>', DeleteCommentView.as_view(), name='delete_comment'),
+
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Article, Comment
 
@@ -14,12 +14,23 @@ class BlogSingleView(View):
     def get(self, request, article_id=1):
         data = Article.objects.get(id=article_id)
         comments = Comment.objects.filter(article=data)
-        return render(request, 'blog/blog-single.html', {"article": data, "comments": comments})
+        return render(request, 'blog/blog-single.html', {"article": data, "comments": comments, "request": request})
 
     def post(self, request, article_id=1):
-        data = Article.objects.get(id=article_id)
-        content = request.POST.get('content')
-        if content:
-            comment = Comment.objects.create(article=data, user=request.user, content=content)
-            comment.save()
-        return redirect('blog:blogsingle', article_id=article_id)
+        if request.user.is_authenticated:
+            data = Article.objects.get(id=article_id)
+            content = request.POST.get('content')
+            if content:
+                comment = Comment.objects.create(article=data, user=request.user, content=content)
+                comment.save()
+            return redirect('blog:blogsingle', article_id=article_id)
+        return redirect('login:login')
+
+
+class DeleteCommentView(View):
+    def get(self, request, comment_id):
+        if request.user.is_authenticated:
+            comment = get_object_or_404(Comment, id=comment_id)
+            if comment.user == request.user:
+                comment.delete()
+        return redirect('blog:blogsingle', article_id=comment.article.id)
